f753f57d44d8f9f8a7f1e5fd358539fa0e85428c
Michael Shih <Michael.Shih@abilitycorp.com.tw>
2019-04-17 16:37:14 +0800

    update for android studio 3.2.1
    update for android studio 3.2.1

--- android/CPC_HYS_mapprc/app/src/main/java/com/example/michael/mapprc/UseXmlStation.java
+++ android/CPC_HYS_mapprc/app/src/main/java/com/example/michael/mapprc/UseXmlStation.java
@@ -4,28 +4,13 @@ package com.example.michael.mapprc;
  * Created by michael on 13/04/15.
  */
 import android.content.res.XmlResourceParser;
-import android.provider.DocumentsContract;
-import android.util.Xml;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 
 public class UseXmlStation {
